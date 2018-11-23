42934e33033d61e62e02436222622be01f45ae32
Escape <yq469@nyu.edu>
2018-11-23 12:32:58 +0800

    purchase update

--- init1.py
+++ init1.py
@@ -16,42 +16,44 @@ conn = pymysql.connect(host='localhost',
 #Search for flights
 @app.route('/searchUpcomingFlights', methods=['GET', 'POST'])
 def searchUpcomingFlights():
-    dept_airport = request.form['dept_airport']
-    dept_city = request.form['dept_city']
-    arr_airport = request.form['arr_airport']
-    arr_city = request.form['arr_city']
-    date = str(request.form['date'])
-    cursor = conn.cursor()
-    control_list=[]
+	dept_airport = request.form['dept_airport']
+	dept_city = request.form['dept_city']
+	arr_airport = request.form['arr_airport']
+	arr_city = request.form['arr_city']
+	date = str(request.form['date'])
+	cursor = conn.cursor()
+	control_list=[]	
+	if dept_airport!='':
+		control_list.append("flight.departure_airport = '%s'"%(dept_airport))
+	if arr_airport!='':
+		control_list.append("flight.arrival_airport = '%s'"%(arr_airport))
+	if date!='':
+		control_list.append("DATE(flight.departure_time) = '%s'"%(date))
+	if dept_city!='':
+		control_list.append("dept.airport_city = '%s'"%(dept_city))
+	if arr_city!='':
+		control_list.append("arr.airport_city = '%s'"%(arr_city))
 	
-    if dept_airport!='':
-        control_list.append("flight.departure_airport = '%s'"%(dept_airport))
-    if arr_airport!='':
-        control_list.append("flight.arrival_airport = '%s'"%(arr_airport))
-    if date!='':
-        control_list.append("DATE(flight.departure_time) = '%s'"%(date))
-    if dept_city!='':
-        control_list.append("dept.airport_city = '%s'"%(dept_city))
-    if arr_city!='':
-        control_list.append("arr.airport_city = '%s'"%(arr_city))
-	
-	
-    if len(control_list)==0:
-        query = "SELECT * \
+	if len(control_list)==0:
+		query = "SELECT * \
 				FROM (flight, airport as dept, airport as arr)\
 				WHERE flight.departure_airport=dept.airport_name and flight.arrival_airport=arr.airport_name AND flight.status = 'upcoming'"
-    else:
-        query = "SELECT * \
+	else:
+		query = "SELECT * \
 				FROM (flight, airport as dept, airport as arr) \
-				WHERE flight.departure_airport=dept.airport_name and flight.arrival_airport=arr.airport_name AND " + " AND ".join(control_list);
-    
-    print(query)
-    cursor.execute(query)
-    data = cursor.fetchall()
-    error = None
-    conn.commit()
-    cursor.close()
-    return render_template('search_results.html', result=data);
+				WHERE flight.departure_airport=dept.airport_name and flight.arrival_airport=arr.airport_name AND flight.status = 'upcoming' AND " + " AND ".join(control_list);
+	cursor.execute(query)
+	data = cursor.fetchall()
+	error = None
+	conn.commit()
+	cursor.close()
+	try:
+		username=session['username'];
+		usertype=session['type'];
+		print(username, usertype)
+		return render_template('search_results.html',result = data, type = 'buy')
+	except:
+		return render_template('search_results.html', result=data);
 	
 @app.route('/backToIndex')
 def backToIndex():
@@ -62,28 +64,27 @@ def backToIndex():
 
 @app.route('/searchFlightStatus', methods=['GET', 'POST'])
 def searchFlightStatus():
-    
-    flight_num = request['flight_num']
-    departure_date = str(request['departure_date'])
-    arrival_date = str(request['arrival_date'])
-    control_list=[]
-    if flight_num!='':
-        control_list.append("flight_num='%d'"%(int(flight_num)))
-    if departure_date != '':
-        control_list.append("DATE(departure_time) = '%s'"%(departure_date))
-    if arrival_date != '':
-        control_list.append("DATE(arrival_time) = '%s'"%(arrival_date))
-    if len(control_list) == 0:
-        query = "SELECT * FROM flight"
-    else:
-        query = "SELECT * FROM flight WHERE " + " AND ".join(control_list)
-    cursor=conn.cursor()
-    cursor.execute(query)
-    data=cursor.fetchall()
-    error = None
-    conn.commit()
-    cursor.close()
-    return render_template('search_results.html',result = data)
+	flight_num = request.form['flight_num']
+	departure_date = str(request.form['departure_date'])
+	arrival_date = str(request.form['arrival_date'])
+	control_list=[]
+	if flight_num!='':
+		control_list.append("flight_num='%d'"%(int(flight_num)))
+	if departure_date != '':
+		control_list.append("DATE(departure_time) = '%s'"%(departure_date))
+	if arrival_date != '':
+		control_list.append("DATE(arrival_time) = '%s'"%(arrival_date))
+	if len(control_list) == 0:
+		query = "SELECT * FROM flight"
+	else:
+		query = "SELECT * FROM flight WHERE " + " AND ".join(control_list)
+	cursor=conn.cursor()
+	cursor.execute(query)
+	data=cursor.fetchall()
+	error = None
+	conn.commit()
+	cursor.close()
+	return render_template('search_results.html',result = data)
 
 @app.route('/register')
 def register():
@@ -115,7 +116,7 @@ def registerCustomer():
 		cursor.execute(ins,(username,name,password,building_number,street,city,state,phone,passport_number,passport_expiration,passport_country,date_of_birth))
 		conn.commit()
 		cursor.close()
-		return render_template('login.html')
+		return render_template('index.html')
 	
 @app.route('/registerStaff', methods=['GET', 'POST'])
 def registerStaff():
@@ -144,7 +145,7 @@ def registerStaff():
 		cursor.execute(ins,(username,password,first_name,last_name,date_of_birth,airline_name))
 		conn.commit()
 		cursor.close()
-		return render_template('login.html')
+		return render_template('index.html')
 		
 @app.route('/registerAgent', methods=['GET', 'POST'])
 def registerAgent():
@@ -165,9 +166,8 @@ def registerAgent():
 		cursor.execute(ins,(username,password,booking_agent_id))
 		conn.commit()
 		cursor.close()
-		return render_template('login.html')
+		return render_template('index.html')
 		
-
 @app.route('/login')
 def login():
 	return render_template('login.html')
@@ -228,67 +228,42 @@ def hello():
 	try:
 		username = session['username']
 		usertype = session['type']
-		print(username, usertype)
 		return render_template('index.html', username=username, usertype=usertype)
 	except:
 		return render_template('index.html');
 
 @app.route('/viewMyFlight', methods=['GET', 'POST'])
 def viewMyFlight():
-    
-    dept_airport = request.form['dept_airport']
-    arr_airport = request.form['arr_airport']
-    start_date = str(request.form['start_date'])
-    end_date = str(request.form['end_date'])
+	dept_airport = request.form['dept_airport']
+	arr_airport = request.form['arr_airport']
+	start_date = str(request.form['start_date'])
+	end_date = str(request.form['end_date'])
+	username = session['username']
+	control_list=[]
+	if dept_airport!='':
+		control_list.append("departure_airport = '%s'"%(dept_airport))
+	if arr_airport!='':
+		control_list.append("arrival_airport = '%s'"%(arr_airport))
+	if start_date!='':
+		control_list.append("DATE(departure_time) > '%s'"%(start_date))
+	if end_date!='':
+		control_list.append("DATE(arrival_time) < '%s'"%(end_date))
+	
+	if len(control_list) == 0:
+		query = "SELECT * FROM flight NATURAL JOIN ticket NATURAL JOIN purchases \
+                WHERE status = 'upcoming' AND customer_email = %s"%(username)
+	else:
+		query = "SELECT * FROM flight NATURAL JOIN ticket NATURAL JOIN purchases \
+                WHERE customer_email = %s AND "%(username) + " AND ".join(control_list)
 
-    control_list=[]
-    if dept_airport!='':
-        control_list.append("departure_airport = '%s'"%(dept_airport))
-    if arr_airport!='':
-        control_list.append("arrival_airport = '%s'"%(arr_airport))
-    if start_date!='':
-        control_list.append("DATE(departure_time) > '%s'"%(start_date))
-    if end_date!='':
-        control_list.append("DATE(arrival_time) < '%s'"%(end_date))
-
-    if len(control_list) == 0:
-        query = "SELECT * FROM flight NATURAL JOIN ticket NATURAL JOIN purchases \
-                WHERE status = 'upcoming'"
-    else:
-        query = "SELECT * FROM flight NATURAL JOIN ticket NATURAL JOIN purchases \
-                WHERE " + " AND ".join(control_list)
-
-    print(query)
-    cursor = conn.cursor()
-    cursor.execute(query)
-    data = cursor.fetchall()
-    error = None
-    return render_template('search_results.html',result = data);
-
-@app.route('/purchaseC1', methods=['GET', 'POST'])
-def purchaseC1():
-    
-    dept_airport = request.form['dept_airport']
-    arr_airport = request.form['arr_airport']
-    departure_date = str(request.form['departure_date'])
-
-    control_list=[]
-    if dept_airport!='':
-        control_list.append("departure_airport = '%s'"%(dept_airport))
-    if arr_airport!='':
-        control_list.append("arrival_airport = '%s'"%(arr_airport))
-    if departure_date!='':
-        control_list.append("DATE(departure_time) = '%s'"%(departure_date))
-    if len(control_list) == 0:
-        query = "SELECT * FROM flight WHERE status = 'upcoming'"
-    else:
-        query = "SELECT * FROM flight WHERE " + " AND ".join(control_list)
-    print(query)
-    cursor = conn.cursor()
-    cursor.execute(query)
-    data = cursor.fetchall()
-    error = None
-    return render_template('search_results.html',result = data, type = "buy");
+	print(query)
+	cursor = conn.cursor()
+	cursor.execute(query)
+	data = cursor.fetchall()
+	conn.commit()
+	cursor.close()
+	error = None
+	return render_template('search_results.html',result = data);
 
 @app.route('/purchaseC2/<airline_name>/<flight_num>',methods=['GET','POST'])
 def purchaseC2(airline_name,flight_num):
